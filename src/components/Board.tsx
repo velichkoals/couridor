8bce3c3e59e3ebe83eee91da9adcf5eed00cba6f
Dima <dmitriy.ivanchenko@corp.sendios.io>
2023-10-07 18:17:43 +0300

    Dima5

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,27 +1,30 @@
-import { useMemo } from "react";
+import { useMemo } from 'react';
 import './Board.css';
 import { Row } from './Row';
-import { generateBoard } from "../helpers/board";
-import { BoardState } from "../model";
-import { Chip } from "../Chip";
+import { generateBoard } from '../helpers/board';
+import { BoardState } from '../model';
+import { Chip } from '../Chip';
 
 type BoardProps = {
-	firstChip: Chip
-	secondChip: Chip
-	boardState: BoardState
-}
+	firstChip: Chip;
+	secondChip: Chip;
+	boardState: BoardState;
+};
 
 export const Board = ({ boardState }: BoardProps) => {
-	const board = useMemo(
-		() => generateBoard(),
-		[]
- 	);
+	const board = useMemo(() => generateBoard(), []);
 
 	return (
-		<div className={'board'}>
-			{board.map((row, rowIndex) => (
-				<Row key={`${rowIndex}`} cells={row} isHorizontal={rowIndex % 2 === 0} />
-			))}
+		<div className='board-container'>
+			<div className={'board'}>
+				{board.map((row, rowIndex) => (
+					<Row
+						key={`${rowIndex}`}
+						cells={row}
+						isHorizontal={rowIndex % 2 === 0}
+					/>
+				))}
+			</div>
 		</div>
 	);
 };
