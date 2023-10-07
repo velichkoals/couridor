4e63d1b2042f660ee0267965a42e5a7d69c29086
Ivan Vakulov <ivan.vakulov@jiji.ng>
2023-10-07 16:11:34 +0300

    Feat: base types

--- src/model.ts
+++ src/model.ts
@@ -1,9 +1,38 @@
 export type Chip = {
-  name: string;
-  walls: number;
-  // index: [number, number]
-  // left(): void;
-  // right(): void;
-  // up(): void;
-  // down(): void;
+    color: boolean
+    bordersLeft: number
+    // left(): void;
+    // right(): void;
+    // up(): void;
+    // down(): void;
 }
+
+export type BoardCell = {
+    x: number;
+    y: number;
+    isCell: boolean
+}
+
+export type Border = {
+    indexFrom: BoardCell
+    indexTo: BoardCell
+}
+
+export type Board = [BoardCell, BoardCell]
+
+export type BoardState = {
+    activeChip: Chip
+    first: BoardCell
+    second: BoardCell
+    borders: Array<Border>
+}
+
+export type BorderStep = {
+    border: Border
+}
+
+export type ChipStep = {
+    index: BoardCell
+}
+
+export type Step = { color: Chip } & BorderStep | ChipStep
