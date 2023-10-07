146cb9efae9d51738fdfc7f6a00238add27058f6
sviatoslav.holovin <svyatoslav.golovin.2005@gmail.com>
2023-10-07 17:48:15 +0300

    update chip

--- src/Chip.ts
+++ src/Chip.ts
@@ -1,7 +1,176 @@
-import { BOARD_SIZE } from './consts';
-import { BoardState, ChipStep, Chip as ChipType } from './model';
+import { BOARD_SIZE, STEP } from './consts';
+import {
+	BoardCell,
+	BoardState,
+	Border,
+	ChipStep,
+	Chip as ChipType,
+} from './model';
 
 // first/top - false
+
+const makeStep = (currentChipCoords: BoardCell, borders: Border[]) => {
+	const steps: ChipStep[] = [];
+
+	if (currentChipCoords.x < BOARD_SIZE) {
+		const newX = currentChipCoords.x + STEP;
+
+		if (
+			!borders.some(
+				(border) =>
+					border.indexFrom.x - 1 === newX &&
+					border.indexFrom.y === currentChipCoords.y &&
+					border.indexTo.x - 1 === newX &&
+					border.indexTo.y === currentChipCoords.y,
+			)
+		) {
+			steps.push({
+				index: {
+					x: newX,
+					y: currentChipCoords.y,
+					isCell: true,
+				},
+			});
+		}
+	}
+
+	if (currentChipCoords.x > 0) {
+		const newX = currentChipCoords.x - STEP;
+
+		if (
+			!borders.some(
+				(border) =>
+					border.indexFrom.x + 1 === newX &&
+					border.indexFrom.y === currentChipCoords.y &&
+					border.indexTo.x + 1 === newX &&
+					border.indexTo.y === currentChipCoords.y,
+			)
+		) {
+			steps.push({
+				index: {
+					x: newX,
+					y: currentChipCoords.y,
+					isCell: true,
+				},
+			});
+		}
+	}
+
+	if (currentChipCoords.y < BOARD_SIZE) {
+		const newY = currentChipCoords.y + STEP;
+
+		if (
+			!borders.some(
+				(border) =>
+					border.indexFrom.x === currentChipCoords.x &&
+					border.indexFrom.y - 1 === newY &&
+					border.indexTo.x === currentChipCoords.x &&
+					border.indexTo.y - 1 === newY,
+			)
+		) {
+			steps.push({
+				index: {
+					x: currentChipCoords.x,
+					y: newY,
+					isCell: true,
+				},
+			});
+		}
+	}
+
+	if (currentChipCoords.y > 0) {
+		const newY = currentChipCoords.y - STEP;
+
+		if (
+			!borders.some(
+				(border) =>
+					border.indexFrom.x === currentChipCoords.x &&
+					border.indexFrom.y + 1 === newY &&
+					border.indexTo.x === currentChipCoords.x &&
+					border.indexTo.y + 1 === newY,
+			)
+		) {
+			steps.push({
+				index: {
+					x: currentChipCoords.x,
+					y: newY,
+					isCell: true,
+				},
+			});
+		}
+	}
+
+	return steps;
+};
+
+const makeJump = (
+	currentChipCoords: BoardCell,
+	otherChipCoords: BoardCell,
+	borders: Border[],
+) => {
+	const steps: ChipStep[] = [];
+
+	if (
+		currentChipCoords.x < BOARD_SIZE &&
+		currentChipCoords.x === otherChipCoords.x - 2
+	) {
+		if (
+			borders.some(
+				(b) =>
+					b.indexFrom.x - 3 === currentChipCoords.x &&
+					b.indexFrom.y === currentChipCoords.y &&
+					b.indexTo.x - 3 === currentChipCoords.x &&
+					b.indexTo.y === currentChipCoords.y,
+			)
+		) {
+			if (
+				!borders.some(
+					(b) =>
+						b.indexFrom.x - 2 === currentChipCoords.x &&
+						b.indexFrom.y - 1 === currentChipCoords.y &&
+						b.indexTo.x - 2 === currentChipCoords.x &&
+						b.indexTo.y - 1 === currentChipCoords.y,
+				)
+			) {
+				steps.push({
+					index: {
+						x: currentChipCoords.x + STEP,
+						y: currentChipCoords.y + STEP,
+						isCell: true,
+					},
+				});
+			}
+
+			if (
+				!borders.some(
+					(b) =>
+						b.indexFrom.x - 2 === currentChipCoords.x &&
+						b.indexFrom.y + 1 === currentChipCoords.y &&
+						b.indexTo.x - 2 === currentChipCoords.x &&
+						b.indexTo.y + 1 === currentChipCoords.y,
+				)
+			) {
+				steps.push({
+					index: {
+						x: currentChipCoords.x + STEP,
+						y: currentChipCoords.y - STEP,
+						isCell: true,
+					},
+				});
+			}
+		} else {
+			steps.push({
+				index: {
+					x: currentChipCoords.x + STEP * 2,
+					y: currentChipCoords.y,
+					isCell: true,
+				},
+			});
+		}
+	}
+
+	return steps;
+};
 
 export class Chip implements ChipType {
 	constructor(
@@ -21,104 +190,17 @@ export class Chip implements ChipType {
 
 		const possibleChipMoves: ChipStep[] = [];
 
-		if (currentChipCoords.x < BOARD_SIZE) {
-			const newX =
-				otherChipCoords.x - 2 === currentChipCoords.x
-					? currentChipCoords.x + 4
-					: currentChipCoords.x + 2;
-
-			if (
-				!borders.some(
-					(border) =>
-						border.indexFrom.x - 1 === newX &&
-						border.indexFrom.y === currentChipCoords.y &&
-						border.indexTo.x - 1 === newX &&
-						border.indexTo.y === currentChipCoords.y,
-				)
-			) {
-				possibleChipMoves.push({
-					index: {
-						x: currentChipCoords.x + 2,
-						y: currentChipCoords.y,
-						isCell: true,
-					},
-				});
-			}
-		}
-
-		if (currentChipCoords.x > 0) {
-			const newX =
-				otherChipCoords.x + 2 === currentChipCoords.x
-					? currentChipCoords.x - 4
-					: currentChipCoords.x - 2;
-
-			if (
-				!borders.some(
-					(border) =>
-						border.indexFrom.x + 1 === newX &&
-						border.indexFrom.y === currentChipCoords.y &&
-						border.indexTo.x + 1 === newX &&
-						border.indexTo.y === currentChipCoords.y,
-				)
-			) {
-				possibleChipMoves.push({
-					index: {
-						x: currentChipCoords.x - 2,
-						y: currentChipCoords.y,
-						isCell: true,
-					},
-				});
-			}
-		}
-
-		if (currentChipCoords.y < BOARD_SIZE) {
-			const newY =
-				otherChipCoords.y - 2 === currentChipCoords.y
-					? currentChipCoords.y + 4
-					: currentChipCoords.y + 2;
-
-			if (
-				!borders.some(
-					(border) =>
-						border.indexFrom.x === currentChipCoords.x &&
-						border.indexFrom.y - 1 === newY &&
-						border.indexTo.x === currentChipCoords.x &&
-						border.indexTo.y - 1 === newY,
-				)
-			) {
-				possibleChipMoves.push({
-					index: {
-						x: currentChipCoords.x,
-						y: currentChipCoords.y + 2,
-						isCell: true,
-					},
-				});
-			}
-		}
-
-		if (currentChipCoords.y > 0) {
-			const newY =
-				otherChipCoords.y + 2 === currentChipCoords.y
-					? currentChipCoords.y - 4
-					: currentChipCoords.y - 2;
-
-			if (
-				!borders.some(
-					(border) =>
-						border.indexFrom.x === currentChipCoords.x &&
-						border.indexFrom.y + 1 === newY &&
-						border.indexTo.x === currentChipCoords.x &&
-						border.indexTo.y + 1 === newY,
-				)
-			) {
-				possibleChipMoves.push({
-					index: {
-						x: currentChipCoords.x,
-						y: currentChipCoords.y - 2,
-						isCell: true,
-					},
-				});
-			}
+		if (
+			currentChipCoords.x !== otherChipCoords.x - 2 ||
+			currentChipCoords.x !== otherChipCoords.x + 2 ||
+			currentChipCoords.y !== otherChipCoords.y - 2 ||
+			currentChipCoords.y !== otherChipCoords.y + 2
+		) {
+			possibleChipMoves.push(...makeStep(currentChipCoords, borders));
+		} else {
+			possibleChipMoves.push(
+				...makeJump(currentChipCoords, otherChipCoords, borders),
+			);
 		}
 
 		return possibleChipMoves;
--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,2 +1,3 @@
-export const BOARD_SIZE = 17
-export const CELLS_COUNT = 9
+export const BOARD_SIZE = 17;
+export const CELLS_COUNT = 9;
+export const STEP = 2;
